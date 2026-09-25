44eff5b390f3b36f64c37f0d328746877ebbae35
Rado-99 <rado.saputra07@gmail.com>
2026-09-25 20:18:50 +0800

    pt2

---
+++ posttest/posttest-apd-2/kelas/pertemuan3.py
@@ -0,0 +1,34 @@
+# praktikum = "apd"
+
+# if praktikum == "apd":
+#     print("kamu lagi mengikuti praktikum apd sekarang")
+# else:
+#     print("kamu mengikuti praktikum lain")
+
+# umur = int(input("Masukkan umur Anda: "))
+# if umur == 17:
+#     print("kamu sudah legal")
+#     print("kamu belum legal")
+
+# kendaraan = input("Masukkan jenis kendaraan: ").lower()
+# if kendaraan == "Mobil":
+#     tarif_parkir = 10000
+# elif kendaraan == "motor":
+#     tarif_parkir = 5000
+# else:
+#     tarif_parkir = 15000
+
+# # Menampilkan tarif parkir yang harus dibayar
+# print("Tarif parkir yang harus dibayar:", tarif_parkir)
+
+# umur = "20"
+# status = "Dewasa" if umur == 20 else "Belum Dewasa"
+# print(status)
+
+# umur = int(input("Masukkan umur Anda: "))
+# if umur >= 16:
+#     print("kamu boleh masuk")
+# else:
+#     print("kamu tidak boleh masuk")
+
+
